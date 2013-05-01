a26604244df76880397292cb8d7316ed81252f32
Aggelos Nikolakopoulos <ang_nikola@yahoo.com>
2013-05-02 02:23:38 +0300

    sample project update

--- app/views/demo/lost_password.php
+++ app/views/demo/lost_password.php
@@ -2,7 +2,7 @@
 	
 		<?php echo validation_errors();?>
 	<form name="input" action="http://mainframe.d/main/validate_lost_password" method="POST">
-	Email: <input type="email" name="e-mail">
+	Type you Email to send your new password: <input type="email" name="e-mail">
 	<input type="submit" value="Recover" name="Recover Password">
     </form> 
 		<a href='<?php echo base_url().'main/login'; ?>'>Back to login page</a>
